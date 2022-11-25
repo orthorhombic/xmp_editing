b704e8f4fc072d6edefdde31715f943969f0bb42
Jason Krizan <34923517+orthorhombic@users.noreply.github.com>
2022-11-24 23:29:10 -0500

    formatting

--- extract_xmp.py
+++ extract_xmp.py
@@ -1,31 +1,30 @@
 import importlib
+import pathlib
+import shutil
 import sqlite3
 
 import pandas as pd
 import pyexiv2
-import seaborn as sns
+import yaml
 from logzero import logger
 from slpp import slpp as lua
-import pathlib
-import yaml
-import shutil
 
 # NOTE: After import into darktable, the metadata "refresh EXIF" button needs to be pressed
 # This will sync the database with the new darktable xmp files
 
 pyexiv2.set_log_level(4)
 logger.setLevel(level="INFO")
-#load settings
+# load settings
 config = importlib.resources.files("untracked").joinpath("config.yml")
 
-with open(config, 'r') as c_file:
+with open(config) as c_file:
     config_data = yaml.load(c_file, Loader=yaml.SafeLoader)
 
-root_path=pathlib.Path(config_data["root_path"])
+root_path = pathlib.Path(config_data["root_path"])
 
 # set up temp folder in tmpfs to keep it in memory
 # /dev/shm is a ramdisk. Create files here for ephemeral transformations that require writing to disk
-temp_dir=pathlib.Path("/dev/shm/xmp_editing")
+temp_dir = pathlib.Path("/dev/shm/xmp_editing")
 temp_dir.mkdir(parents=True, exist_ok=True)
 
 # load tags darktable can process:
@@ -36,7 +35,7 @@ with importlib.resources.files("tags").joinpath("tags_from_darktable.txt").open(
     tags = f.read().splitlines()
 
 # test query of the view created in img_view.sql
-sql_query="""
+sql_query = """
 select * from IMG
 WHERE baseName like 'Crystal-0075%'
 or baseName like 'Crystals-1600%'
@@ -46,7 +45,7 @@ or baseName like 'LWP42267%'
 
 """
 
-#Creating the path to the lightroom catalog
+# Creating the path to the lightroom catalog
 catalog = importlib.resources.files("untracked").joinpath("LightroomCatalog.lrcat")
 
 # Create your connection.
@@ -57,11 +56,10 @@ df = pd.read_sql_query(sql_query, cnx)
 cnx.close()
 
 
-
-
-
-def parse_lightroom_processtext(lightroom_processtext:str, tags:list[str], process_ver:float=6.7):
-    """Process lightroom parameters compatible with darktable. 
+def parse_lightroom_processtext(
+    lightroom_processtext: str, tags: list[str], process_ver: float = 6.7
+):
+    """Process lightroom parameters compatible with darktable.
     See https://github.com/darktable-org/darktable/blob/master/src/develop/lightroom.c for info."""
 
     if lightroom_processtext[0:4] == "s = ":
@@ -77,12 +75,12 @@ def parse_lightroom_processtext(lightroom_processtext:str, tags:list[str], proce
 
     if "Xmp.crs.ToneCurvePV2012" in intersect_dict.keys():
         # based on the darktable code, it looks like this is expected to be a list of tuples/lists
-        temp_list=intersect_dict["Xmp.crs.ToneCurvePV2012"]
-        if len(temp_list)%2==1:
+        temp_list = intersect_dict["Xmp.crs.ToneCurvePV2012"]
+        if len(temp_list) % 2 == 1:
             raise ValueError("Unexpected length of ToneCurvePV2012")
 
-        new_list=[]
-        for i in range(len(temp_list)//2):
+        new_list = []
+        for i in range(len(temp_list) // 2):
             # pyexiv2 uses ", " as a separator for multiple values.
             # So it might automatically split the string you want to write.
             # https://github.com/LeoHsiao1/pyexiv2/blob/master/docs/Tutorial.md
@@ -90,24 +88,22 @@ def parse_lightroom_processtext(lightroom_processtext:str, tags:list[str], proce
 
         intersect_dict["Xmp.crs.ToneCurvePV2012"] = new_list
 
-
     return intersect_dict
 
 
-
-def copy_xmp_temp(from_file:pathlib.PosixPath, to_file:pathlib.PosixPath):
+def copy_xmp_temp(from_file: pathlib.PosixPath, to_file: pathlib.PosixPath):
     try:
         # open source file
         with pyexiv2.Image(from_file.as_posix()) as img:
             file_raw_xmp = img.read_raw_xmp()
         # write data to temp
-        with open(to_file, 'w') as f:
+        with open(to_file, "w") as f:
             f.write(file_raw_xmp)
     except RuntimeError:
         logger.debug(f"file not found: {from_file}")
 
 
-def extend_xmp(base:pyexiv2.core.Image,xmp_file:pathlib.PosixPath):
+def extend_xmp(base: pyexiv2.core.Image, xmp_file: pathlib.PosixPath):
 
     with pyexiv2.Image(xmp_file.as_posix()) as img:
         file_xmp = img.read_xmp()
@@ -118,46 +114,34 @@ def extend_xmp(base:pyexiv2.core.Image,xmp_file:pathlib.PosixPath):
     return base
 
 
-# i=3
-# temp_path=df.loc[i,"BaseName"]+"."+df.loc[i,"FileType"]
-# lr_xmp_path=df.loc[i,"BaseName"]+".xmp"
-# darktable_xmp_path=df.loc[i,"BaseName"]+"."+df.loc[i,"FileType"]+".xmp"
-# lightroom_processtext=df.loc[i,"processtext"]
-# process_ver=df.loc[i,"processversion"]
-# db_xmp=df.loc[i,"xmp"]
-
-# data_series=df.loc[i,:]
-
-
 def process_file(data_series):
 
-    temp_path=data_series.loc["BaseName"]+"."+data_series.loc["FileType"]
-    lr_xmp_path=data_series.loc["BaseName"]+".xmp"
-    darktable_xmp_path=data_series.loc["BaseName"]+"."+data_series.loc["FileType"]+".xmp"
-    lightroom_processtext=data_series.loc["processtext"]
-    process_ver=data_series.loc["processversion"]
-    db_xmp=data_series.loc["xmp"]
+    temp_path = data_series.loc["BaseName"] + "." + data_series.loc["FileType"]
+    lr_xmp_path = data_series.loc["BaseName"] + ".xmp"
+    darktable_xmp_path = (
+        data_series.loc["BaseName"] + "." + data_series.loc["FileType"] + ".xmp"
+    )
+    lightroom_processtext = data_series.loc["processtext"]
+    process_ver = data_series.loc["processversion"]
+    db_xmp = data_series.loc["xmp"]
 
     # combine path
-    filepath=pathlib.Path(root_path,temp_path)
-    filepath_lr_xmp=pathlib.Path(root_path,lr_xmp_path)
-    filepath_darktable_xmp=pathlib.Path(root_path,darktable_xmp_path)
+    filepath = pathlib.Path(root_path, temp_path)
+    filepath_lr_xmp = pathlib.Path(root_path, lr_xmp_path)
+    filepath_darktable_xmp = pathlib.Path(root_path, darktable_xmp_path)
 
-    #check the file first, abort if it isn't there
+    # check the file first, abort if it isn't there
     if not filepath.is_file():
         logger.info(f"File {filepath} not found")
         return
-        
 
-
-    # paths: original file xmp copy, database xmp, sidecar file.xmp, sidecar file.ext.xmp, 
-    temp_files={
-        "orig":pathlib.Path(temp_dir,"orig.xmp"),
-        "db":pathlib.Path(temp_dir,"db.xmp"),
-        "sidecar_lr":pathlib.Path(temp_dir,"sidecar.xmp"),
-        "sidecar_darktable":pathlib.Path(temp_dir,"sidecar.ext.xmp"),
+    # paths: original file xmp copy, database xmp, sidecar file.xmp, sidecar file.ext.xmp,
+    temp_files = {
+        "orig": pathlib.Path(temp_dir, "orig.xmp"),
+        "db": pathlib.Path(temp_dir, "db.xmp"),
+        "sidecar_lr": pathlib.Path(temp_dir, "sidecar.xmp"),
+        "sidecar_darktable": pathlib.Path(temp_dir, "sidecar.ext.xmp"),
     }
-
 
     # Create temp files from extracted data
 
@@ -166,9 +150,8 @@ def process_file(data_series):
     else:
         raise FileNotFoundError(f"Not found: {filepath}")
 
-
     # write database XMP to temp
-    with open(temp_files["db"], 'w') as f:
+    with open(temp_files["db"], "w") as f:
         f.write(db_xmp)
 
     # try sidecar files
@@ -177,29 +160,23 @@ def process_file(data_series):
     copy_xmp_temp(filepath_darktable_xmp, temp_files["sidecar_darktable"])
 
     # prepare data from database for stacking
-    intersect_dict=parse_lightroom_processtext(lightroom_processtext=lightroom_processtext, tags=tags, process_ver=process_ver)
-
+    intersect_dict = parse_lightroom_processtext(
+        lightroom_processtext=lightroom_processtext, tags=tags, process_ver=process_ver
+    )
 
     # stack data
     # stack all XMP data file, sidecar, db_xmp, then add processtext
     new_xmp = pyexiv2.Image(temp_files["orig"].as_posix())
 
-
-
-    new_xmp=extend_xmp(new_xmp,temp_files["db"])
+    new_xmp = extend_xmp(new_xmp, temp_files["db"])
     if temp_files["sidecar_lr"].is_file():
-        new_xmp=extend_xmp(new_xmp,temp_files["sidecar_lr"])
+        new_xmp = extend_xmp(new_xmp, temp_files["sidecar_lr"])
     if temp_files["sidecar_darktable"].is_file():
-        new_xmp=extend_xmp(new_xmp,temp_files["sidecar_darktable"])
-
-
+        new_xmp = extend_xmp(new_xmp, temp_files["sidecar_darktable"])
 
     new_xmp.modify_xmp(intersect_dict)
 
-
-
     # copy data back to LR format file
-
 
     if filepath_lr_xmp.is_file():
         # update
@@ -210,32 +187,28 @@ def process_file(data_series):
         # copy
         shutil.copy(temp_files["orig"], filepath_lr_xmp)
 
-
     new_xmp.close()
-
 
     # final file cleanup
 
     # if the label is none, remove it. This would otherwise get set as a purple label
     # <xmp:Label>None</xmp:Label>
 
-
-    #need check to confirm there is an exif block with resolution in it
+    # need check to confirm there is an exif block with resolution in it
     # if HasCrop:
     #     needs:
     #         ImageWidth
     #         ImageLength
     #         Orientation
 
-
     # delete files
     # Cleanup temp files:
-    for key,val in temp_files.items():
+    for key, val in temp_files.items():
         try:
             val.unlink()
         except FileNotFoundError:
             logger.debug(f"file not found for cleanup: {key}, {val}")
-            pass
+
 
 for i, data_series in df.iterrows():
     logger.debug("Index: {i}")
