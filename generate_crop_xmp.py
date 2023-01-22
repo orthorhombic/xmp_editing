030388c71548fa7c6c174e2baeb925a03875097c
Jason Krizan <34923517+orthorhombic@users.noreply.github.com>
2023-01-21 23:01:19 -0500

    updates with config file and loop over files

--- generate_crop_xmp.py
+++ generate_crop_xmp.py
@@ -1,6 +1,7 @@
 # inspired by:
 # https://github.com/z80z80z80/autocrop
 # https://github.com/smc8050/Dias_Autocrop
+import importlib.resources
 import pathlib
 import tempfile
 from shutil import copy2
@@ -9,6 +10,7 @@ import cv2
 import logzero
 import pyexiv2
 import rawpy
+import yaml
 from exiftool import ExifTool
 from logzero import logger
 from PIL import Image
@@ -26,121 +28,161 @@ default_device = cv2.ocl.Device_getDefault()
 
 logger.debug(f"Default device: {default_device.name()}")
 
-# img_in_path = "untracked/test/raw0032.dng"
+config = importlib.resources.files("untracked").joinpath("crop_config.yml")
 
-img_name = "raw0032.dng"
+with open(config) as c_file:
+    config_data = yaml.load(c_file, Loader=yaml.SafeLoader)
 
-crop_addition = -5
-
-debug = True
-
-# root_path = pathlib.Path(config_data["root_path"])
-
-root_path = pathlib.Path("untracked/test")
-img_filename = "raw0032.dng"
-filepath = pathlib.Path(root_path, img_filename)
+root_path = pathlib.Path(config_data["root_path"])
+debug = config_data["debug"]
+crop_addition = config_data["crop_addition"]
 
 if debug:
     debug_path = pathlib.Path(root_path, "debug")
     debug_path.mkdir(parents=True, exist_ok=True)
 
 
-with rawpy.imread(filepath.as_posix()) as raw:
-    imcv2 = raw.postprocess()
+def process_file(
+    filepath: pathlib.Path,
+    debug_path: pathlib.Path,
+    debug: bool,
+    crop_addition: int,
+    et: ExifTool,
+):
 
-tempdir = tempfile.TemporaryDirectory(dir="/dev/shm")
-temp_dir_path = pathlib.Path(tempdir.name)
+    if filepath.suffix.upper() in xmp_editing_utils.raw_files:
+        logger.debug("Processing as raw file")
+        with rawpy.imread(filepath.as_posix()) as raw:
+            imcv2 = raw.postprocess()
+    elif filepath.suffix.upper() in xmp_editing_utils.other_files:
+        logger.debug("Processing as regular file. Extension not in raw_files list.")
+        imcv2 = cv2.imread(filepath.as_posix())
+    else:
+        logger.error("Filetype not supported")
+        return
 
-temp_xmp_path = pathlib.Path(temp_dir_path, "orig.xmp")
-base_name = img_name.split(".")[0]
-filepath_lr_xmp = pathlib.Path(root_path, base_name + ".xmp")
+    img = Image.fromarray(
+        cv2.cvtColor(imcv2, cv2.COLOR_BGR2RGB)
+    )  # convert from cv2 image file to pil image file
+    original_img = img
+    w, h = original_img.size
+    blurred_img = img.filter(
+        ImageFilter.GaussianBlur(radius=4)
+    )  # to remove outlier pixels
+    binary_img = xmp_editing_utils.convert_to_binary(blurred_img, 45, 255)
+    bg = Image.new(binary_img.mode, binary_img.size)
+    diff = ImageChops.difference(binary_img, bg)
+    bbox = diff.getbbox()
+    # left, top, right, bottom
+    # 0%, 0%, 100%, 100%
 
-# Create temp files from extracted data
-with ExifTool() as et:
-    xmp_editing_utils.copy_xmp_temp(filepath, temp_xmp_path, et=et)
+    if bbox == (0, 0, w, h):
+        logger.error(f"No cropping detected for {filepath.as_posix()}")
+        return
+    elif bbox:
+        color_control = xmp_editing_utils.get_control_value(original_img, bbox)
+        if color_control > 15:
+            logger.warning(f"Crop bounds may be problematic for {filepath.as_posix()}")
+    else:
+        raise RuntimeError("Could not find a bounding box for crop")
 
-# imcv2 = cv2.imread(filepath)
+    # adjust based on cropp_addition
 
-# TODO: Rotation before cropping has to be optimised (more accuracy)
-# theta = get_rotation(imcv2)
-# imcv2 = rotate_image(imcv2, 180 * theta / np.pi - 90)
+    if bbox[0] + crop_addition > 0:
+        left = bbox[0] + crop_addition
+    else:
+        left = 0
 
-img = Image.fromarray(
-    cv2.cvtColor(imcv2, cv2.COLOR_BGR2RGB)
-)  # convert from cv2 image file to pil image file
-original_img = img
-w, h = original_img.size
-blurred_img = img.filter(ImageFilter.GaussianBlur(radius=4))  # to remove outlier pixels
-binary_img = xmp_editing_utils.convert_to_binary(blurred_img, 40, 255)
-bg = Image.new(binary_img.mode, binary_img.size)
-diff = ImageChops.difference(binary_img, bg)
-bbox = diff.getbbox()
-# left, top, right, bottom
-# 0%, 0%, 100%, 100%
+    if bbox[1] + crop_addition > 0:
+        top = bbox[1] + crop_addition
+    else:
+        top = 0
 
-if bbox:
-    color_control = xmp_editing_utils.get_control_value(original_img, bbox)
-    if color_control > 1.5:
-        logger.warning(f"Crop bounds may be problematic for {filepath.as_posix()}")
-else:
-    raise RuntimeError("Could not find a bounding box for crop")
+    if bbox[2] - crop_addition < w:
+        right = bbox[2] - crop_addition
+    else:
+        right = w
 
+    if bbox[3] - crop_addition < h:
+        bottom = bbox[3] - crop_addition
+    else:
+        bottom = h
 
-# adjust based on cropp_addition
+    new_box = (left, top, right, bottom)
 
-if bbox[0] + crop_addition > 0:
-    left = bbox[0] + crop_addition
-else:
-    left = 0
+    crop_parm = {}
 
-if bbox[1] + crop_addition > 0:
-    top = bbox[1] + crop_addition
-else:
-    top = 0
+    crop_parm["Xmp.crs.HasCrop"] = "True"
+    crop_parm["Xmp.crs.CropLeft"] = left / w
+    crop_parm["Xmp.crs.CropTop"] = top / h
+    crop_parm["Xmp.crs.CropRight"] = right / w
+    crop_parm["Xmp.crs.CropBottom"] = bottom / h
+    crop_parm["Xmp.crs.CropAngle"] = 0
+    crop_parm["Xmp.tiff.ImageWidth"] = w
+    crop_parm["Xmp.tiff.ImageLength"] = h
+    crop_parm["Xmp.tiff.Orientation"] = 1
 
-if bbox[2] - crop_addition < w:
-    right = bbox[2] - crop_addition
-else:
-    right = w
+    if filepath.suffix.upper() == ".DNG":
+        crop_parm["Xmp.crs.Exposure2012"] = -0.01
 
-if bbox[3] - crop_addition < h:
-    bottom = bbox[3] - crop_addition
-else:
-    bottom = h
+    tempdir = tempfile.TemporaryDirectory(dir="/dev/shm")
+    temp_dir_path = pathlib.Path(tempdir.name)
 
-new_box = (left, top, right, bottom)
+    temp_xmp_path = pathlib.Path(temp_dir_path, "orig.xmp")
+    filepath_lr_xmp = filepath.with_suffix(".xmp")
 
-crop_parm = {}
+    # Create temp files from extracted data
+    with ExifTool() as et:
+        xmp_editing_utils.copy_xmp_temp(filepath, temp_xmp_path, et=et)
 
-crop_parm["Xmp.crs.HasCrop"] = "True"
-crop_parm["Xmp.crs.CropLeft"] = left / w
-crop_parm["Xmp.crs.CropTop"] = top / h
-crop_parm["Xmp.crs.CropRight"] = right / w
-crop_parm["Xmp.crs.CropBottom"] = bottom / h
-crop_parm["Xmp.crs.CropAngle"] = 0
-crop_parm["Xmp.crs.Exposure2012"] = -0.01
-crop_parm["Xmp.tiff.ImageWidth"] = w
-crop_parm["Xmp.tiff.ImageLength"] = h
-crop_parm["Xmp.tiff.Orientation"] = 1
+    # write xmp
+    with pyexiv2.Image(temp_xmp_path.as_posix()) as img:
+        img.modify_xmp(crop_parm)
+        img.read_xmp()
 
-# write xmp
-with pyexiv2.Image(temp_xmp_path.as_posix()) as img:
-    img.modify_xmp(crop_parm)
-    final_xmp_dict = img.read_xmp()
+    copy2(temp_xmp_path, filepath_lr_xmp)
 
+    tempdir.cleanup()
 
-copy2(temp_xmp_path, filepath_lr_xmp)
+    if debug:
+        im = xmp_editing_utils.draw_cropline(original_img, new_box)
+        max_size = 800
+        scale_factor = max_size / max([w, h])
+        small_w, small_h = int(w * scale_factor), int(h * scale_factor)
+
+        im = im.resize((small_w, small_h))
+
+        debug_filename = pathlib.Path(debug_path, filepath.name + ".jpg")
+        im.save(debug_filename, quality=100, subsampling=0)
 
 
-tempdir.cleanup()
+def main():
 
-if debug:
-    im = xmp_editing_utils.draw_cropline(original_img, new_box)
-    max_size = 800
-    scale_factor = max_size / max([w, h])
-    small_w, small_h = int(w * scale_factor), int(h * scale_factor)
+    p = root_path.glob("*")
+    files = [x for x in p if x.is_file()]
 
-    im = im.resize((small_w, small_h))
+    supported_files = set.union(
+        set(xmp_editing_utils.raw_files), set(xmp_editing_utils.other_files)
+    )
+    files = [x for x in files if x.suffix.upper() in supported_files]
 
-    debug_filename = pathlib.Path(debug_path, img_filename + ".jpg")
-    im.save(debug_filename, quality=100, subsampling=0)
+    with ExifTool() as et:
+        for filepath in files:
+            # kept in a simple for loop because limited by network bandwidth
+            logger.info(f"Processing: {filepath}")
+            try:
+                process_file(
+                    filepath=filepath,
+                    debug_path=debug_path,
+                    debug=debug,
+                    crop_addition=crop_addition,
+                    et=et,
+                )
+            except Exception as e:
+                logger.error(f"Failed to process: {filepath}")
+                logger.error(f"Exception: {e}")
+
+
+if __name__ == "__main__":
+    logger.info("Running main()")
+    main()
